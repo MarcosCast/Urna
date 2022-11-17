c9fe8c7ec65fbec879198407c88e5b2abda44a9e
Marcos Castro <marcosmax.5@gmail.com>
2022-11-16 21:39:34 -0300

    Iniciando as funções

---
+++ fases.js
@@ -0,0 +1,16 @@
+let fases = [
+    {
+    titulo: 'DEPUTADO DISTRITAL',
+    numeros: 5,
+    candidatos: [
+        {
+            numero: 19111,
+            name: 'Zé Carlos',
+            partido: 'Butecão',
+        
+
+        },
+    
+
+]
+    }

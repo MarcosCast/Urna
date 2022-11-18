b05b33478044a1f6434a0964e215c13e2463171a
Marcos Castro <marcosmax.5@gmail.com>
2022-11-17 21:14:17 -0300

    Iniciando as funções no script

--- fases.js
+++ fases.js
@@ -1,22 +1,52 @@
 let fases = [
     {
-    titulo: 'DEPUTADO DISTRITAL',
-    numeros: 5,
-    candidatos: [
+     titulo: 'DEPUTADO DISTRITAL',
+     numeros: 5,
+     candidatos: [
         {
-            numero: 19111,
-            name: 'Zé Carlos',
+            numero: 15669,
+            nome: 'Napoleão Bonaparte',
             partido: 'Nacionalista',
             fotos:[
-                {url}
+                {url:'15669.jpg', legenda: 'Deputato Distrital', small: fake}
             ]    
         },
         {
-            numero: 20001,
-            name: ''
-        }
-    
-
-]
+            numero: 21155,
+            nome: 'Maria Antonieta',
+            partido: 'Liberdad',
+            fotos:[
+                {url:'21155.jpg', legenda: 'Deputato Distrital', small: fake}
+            ]
+        },   
+               ]
+    },
+    {
+        titulo: 'GOVERNADOR',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 97,
+                nome: 'Pedro',
+                partido: 'Conquiste sua Vaga',
+                vice: 'Henrique',
+                fotos:[
+                    {url:'97.jpg', legenda: 'Governador'},
+                    {url:'97_1.jpg', legenda: 'Vice-Governador', small: true}
+                ]
+            },
+            {
+                numero: 90,
+                nome: 'Heder',
+                partido: 'Tutores',
+                vice: 'Renato',
+                fotos: [
+                    {url:'90.jpg', legenda: 'Governador'},
+                    {url:'90_1.jpg', legenda: 'Vice-Governador', small: true}
+                ]
+            },
+        ]
+        
     }
-]
+
+];
---
+++ script.js
@@ -0,0 +1,4 @@
+let seuVotoPara = document.querySelector('.p1a span');
+let cargo = document.querySelector('.p1b span');
+let descricao = document.querySelector('.p1d');
+let aviso = document.querySelector('.p2');
